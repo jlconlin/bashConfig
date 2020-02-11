de59c34b5dbbd91d58f2eb984a6c11d3d6ad477c
Jeremy Conlin <jlconlin@lanl.gov>
2020-02-11 10:52:50 -0700

    Improving powerline configuration.

--- bash/powerline.bash
+++ bash/powerline.bash
@@ -1,9 +1,10 @@
-
 export POWERLINE_ROOT=$(pip show powerline-status | sed -n 's/Location: \(.*\)/\1/p')
+
 # Test if we have powerline installed
 if [ $? -eq 0 ]; then
+  powerline-daemon -q
   . $POWERLINE_ROOT/powerline/bindings/bash/powerline.sh
-  powerline-config tmux setup
+  # powerline-config tmux setup
   export POWERLINE_BASH_CONTINUATION=1
   export POWERLINE_BASH_SELECT=1
 fi
